da058e59bdd2d7c88577d41b606fde905bcb9dec
akshaybhaltilak <99380793+akshaybhaltilak@users.noreply.github.com>
2024-10-25 10:09:48 +0530

    first

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ app.use(cors()); // Allow CORS for your frontend
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Update this with your frontend URL
+    origin: 'https://sanchat.vercel.app/', // Update this with your frontend URL
     methods: ['GET', 'POST'],
   },
 });
